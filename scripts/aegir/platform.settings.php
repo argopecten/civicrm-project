c2f1eb62954065cce3edff1a863f9ffd771ecb74
petergerner <pgerner@gmail.com>
2020-04-24 18:43:53 +0200

    add settings.default.json

--- scripts/aegir/platform.settings.php
+++ scripts/aegir/platform.settings.php
@@ -3,7 +3,7 @@
 /*
  * Define CIVICRM_L10N_BASEDIR environmental variable for l10n files
  * see: https://lab.civicrm.org/dev/translation/-/issues/30
- * PLATFORM_DIR are be replaced at install time
+ * PLATFORM_DIR will be / has been replaced during install
  * sed -i "s/PLATFORM_DIR/$(basename $PWD)/g" scripts/aegir/platform.settings.php
  */
 define('CIVICRM_L10N_BASEDIR', '/var/aegir/platforms/PLATFORM_DIR/web/profiles/civicrmprofile/translations/contrib/civicrm/l10n');
