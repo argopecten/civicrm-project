2ff72f05f987bf2373d0fed2a619a86e611d24bb
Gerner Péter <petergerner@users.noreply.github.com>
2022-09-04 19:31:50 +0200

    use argopecten/civicrmprofile 5.52.3+9.4.5 (#30)
    * Update composer.lock
    * argopecten/civicrmprofile: 5.52.3+9.4.5

--- scripts/install-l10n/deploy-civicrm-l10n.sh
+++ scripts/install-l10n/deploy-civicrm-l10n.sh
@@ -17,11 +17,11 @@ cp /tmp/civicrm/sql/*.hu_HU.mysql vendor/civicrm/civicrm-core/sql/
 # l10n settings file: settings.default.json
 # quick & dirty, needed until not included in official l10n repo
 # https://lab.civicrm.org/dev/translation/-/merge_requests/3
-cp -rl scripts/install-l10n/settings.default.json vendor/civicrm/civicrm-core/l10n/hu_HU
+# cp -rl scripts/install-l10n/settings.default.json vendor/civicrm/civicrm-core/l10n/hu_HU
 
 # define CIVICRM_L10N_BASEDIR environmental variable as platform settings in Aegir
-cp -rl scripts/install-l10n/platform.settings.php web/sites/all/
-sed -i "s/PLATFORM_DIR/$(basename $PWD)/g" web/sites/all/platform.settings.php
+# cp -rl scripts/install-l10n/platform.settings.php web/sites/all/
+# sed -i "s/PLATFORM_DIR/$(basename $PWD)/g" web/sites/all/platform.settings.php
 
 # clean up
 rm -rf /tmp/l10n.tar.gz /tmp/civicrm
